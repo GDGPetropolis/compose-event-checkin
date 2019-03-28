5b08e70c8f171493365dff2103b6e4325a10711a
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-03-28 16:28:03 -0300

    corrigido restore

--- restore_database.sh
+++ restore_database.sh
@@ -2,4 +2,5 @@
 
 DOCKER_ID=`docker ps -aqf "name=composeeventcheckin_database_1"`
 
+cat clean_data.sql | docker exec -i "${DOCKER_ID}" mysql -u root --password=supersecret GdgDatabase
 cat gdg_database.sql | docker exec -i "${DOCKER_ID}" mysql -u root --password=supersecret GdgDatabase
